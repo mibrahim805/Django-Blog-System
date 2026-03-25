67d8520892f2dc68297fd9972f49c548e6955462
m ibrahim <mibrahimjutt1266@gmail.com>
2026-03-25 11:44:37 +0500

    consumer settings

--- BlogSystem2/asgi.py
+++ BlogSystem2/asgi.py
@@ -13,4 +13,22 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlogSystem2.settings')
 
-application = get_asgi_application()
+# application = get_asgi_application()
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+import blog.routing
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            blog.routing.websocket_urlpatterns
+        )
+    ),
+})
